e26badc8409a3833a0734c76f3791276d7e2bdfc
Ramy Ben Aroya <ramybenaroya@gmail.com>
2018-04-25 04:44:46 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# Ember Inject Config
+
+Injection to Ember CLI app configuration at runtime.
+
+## What is it for?
+Well, sometimes you just want to change ember's config (`app/config/environment.js`) before everything is loaded, or even extract it programmatically from Local Storage or a Cookie.
+
+## Ok, how?
+
+First you need to import `EmberApp` from a different location:
+
+```javascript
+// ember-cli-build.js
+
+const EmberApp = require('ember-inject-config/ember-app');
+
+module.exports = function (defaults) {
+	const app = new EmberApp(defaults, {
+        // ...
+
+        // Add this in case the meta config tag's content is serialized with Base64 (which is by default by ember-cli)
+        'ember-inject-config': {
+            isBase64: true
+        }
+    }
+}
+
+```
+Then, add an inline script in `app/index.html` (before the app & vendor scripts):
+```html
+<!-- app/index.html -->
+<!DOCTYPE html>
+{{content-for 'rev-header'}}
+<html>
+  <!-- ... -->
+  <body>
+    <!-- ... -->
+    <script>
+        window.configToInject = {
+            property1: 'value1',
+            property2: 'value2',
+            property3: {
+                someKey: 'someValue'
+            },
+            property4: localStorage.getItem('property4')
+        };
+    </script>
+    <!-- ... -->
+  </body>
+</html>
+```
+
+The content of `window.configToInject` will be **DEEPLY MERGED** into the module `<your-app-name>/config/environment.js` at runtime, before the app is initialized (even before initializers).
+
+## Options
+Options can be passed within the `ember-inject-config` node of `EmberApp` options object.
+
+#### `globalVarName`
+Type: `String`  
+Default: `'configToInject'`   
+The global variable name to inject the config from.
+
+#### `isBase64`
+Type: `Boolean`   
+Default: `false`   
+Specify whether the meta config tag's content is serialized with Base64 (which is by default by ember-cli).  
---
+++ ember-app.js
@@ -0,0 +1,73 @@
+const OfficialEmberApp = require("ember-cli/lib/broccoli/ember-app");
+
+class EmberApp extends OfficialEmberApp {
+	constructor(...args) {
+		super(...args);
+    }
+    
+	_contentForConfigModule(content, config) {
+		let options = this.options && this.options["ember-inject-config"];
+		let injectedContent = getInjectedContent(options);
+		let returnIndex;
+		let appConfigFromMeta;
+
+		super._contentForConfigModule(content, config);
+
+		appConfigFromMeta = content[content.length - 1].toString();
+		returnIndex = appConfigFromMeta.indexOf("return ");
+		appConfigFromMeta =
+			appConfigFromMeta.substring(0, returnIndex) +
+			injectedContent +
+			appConfigFromMeta.substring(returnIndex, appConfigFromMeta.length);
+		content[content.length - 1] = appConfigFromMeta;
+	}
+}
+
+if (typeof EmberApp.env === "function") {
+	RAMPApp.env = EmberApp.env;
+}
+
+function getInjectedContent(options) {
+	options = options || {};
+	var varName = options.globalVarName || "configToInject",
+		isBase64 = !!(options && options.isBase64),
+		injectedContent = `var parsedConfigToInject;
+var deepExtend = function(out) {
+  out = out || {};
+
+  for (var i = 1, len = arguments.length; i < len; ++i) {
+    var obj = arguments[i];
+
+    if (!obj) {
+      continue;
+    }
+
+    for (var key in obj) {
+      if (!obj.hasOwnProperty(key)) {
+        continue;
+      }
+      if (Object.prototype.toString.call(obj[key]) === '[object Object]') {
+        out[key] = deepExtend(out[key], obj[key]);
+        continue;
+      }
+
+      out[key] = obj[key];
+    }
+  }
+
+  return out;
+};
+	if (window["${varName}"]) {
+		try {
+			parsedConfigToInject = window["${varName}"];
+			${isBase64 ? `if (typeof window["${varName}"] === "string") {
+				parsedConfigToInject = JSON.parse(atob(window["${varName}"]));
+			}` : ""}
+			deepExtend(config, parsedConfigToInject);
+		} catch (e) {}
+	}
+	`;
+	return injectedContent;
+}
+
+module.exports = EmberApp;
